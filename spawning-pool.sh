3d9f854bd605ae301ac47e0b73373952ca134d59
ferris <jake@outracks.com>
2015-02-26 00:14:50 +0100

    dropped lcd speed

--- spawning-pool.sh
+++ spawning-pool.sh
@@ -5,7 +5,7 @@ REPO_URI=https://github.com/notro/rpi-firmware rpi-update
 
 # Smack my bitch up
 printf 'spi-bcm2708\nstmpe_ts\ndtoverlay=pitft\n' >> /boot/config.txt
-sed -i '1 s/$/ fbcon=map:10/' /boot/cmdline.txt
+sed -i '1 s/$/ fbcon=map:10 fbtft_device.speed=10000000/' /boot/cmdline.txt
 
 # Install Hive server (will reboot when done)
 git clone https://github.com/yupferris/TheHiveLives.git
