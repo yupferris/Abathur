ac9610c9deb9af9b2e07066f59cbd65c2ff713d5
ferris <jake@outracks.com>
2015-02-26 10:43:26 +0100

    added htop

--- spawning-pool.sh
+++ spawning-pool.sh
@@ -6,6 +6,7 @@ REPO_URI=https://github.com/notro/rpi-firmware rpi-update
 # Smack my bitch up
 printf 'spi-bcm2708\nstmpe_ts\ndtoverlay=pitft\n' >> /boot/config.txt
 sed -i '1 s/$/ fbcon=map:10 fbtft_device.speed=10000000/' /boot/cmdline.txt
+apt-get -y install htop
 
 # Install Hive server (will reboot when done)
 git clone https://github.com/yupferris/TheHiveLives.git
